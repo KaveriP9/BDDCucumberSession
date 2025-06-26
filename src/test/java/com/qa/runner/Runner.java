44814176b8012a96d11c4d3c1960c859ff2bf0c1
KaveriP9 <pKawery@gmail.com>
2025-06-26 15:23:41 +0530

    Updated runner file is committed

--- src/test/java/com/qa/runner/Runner.java
+++ src/test/java/com/qa/runner/Runner.java
@@ -5,13 +5,17 @@ import cucumber.api.junit.Cucumber;
 import cucumber.api.CucumberOptions;
 
 /**
- * @author this is the runnerclass to run the feature and step defination and generate report
+ * @author this is the runner class to run the feature and step defination and generate report
  *
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = { "com.qa.stepdefinations" },  plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" }, monochrome = true)
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.qa.stepdefinations" },
+tags = { "@employeeCRUD5TCS" },  plugin = { "pretty", "html:target/cucumber-reports/Cucumber.html" }, monochrome = true)
 
 public class Runner {
 
 
 }
+
+//for json report- json:target/cucumber-reports/Cucumber.json
+//for html report- html:target/cucumber-reports/Cucumber.html
